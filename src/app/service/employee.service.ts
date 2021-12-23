c53fc7025c4c6574ace60b0059ce100f71f0ab75
DVorobiev1968 <vdv@rdturbo.ru>
2021-12-23 17:19:40 +0500

    Пишем реализацию сервисов наших Entity: user.service.ts employee.service.ts

--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+const USER_API = 'http://localhost:8090/api/employee/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getEmployeeById(id: number): Observable<any> {
+    return this.http.get(USER_API +'id/' +id);
+  }
+
+  updateEmployee(employee: any): Observable<any> {
+    return this.http.post(USER_API + 'update', employee);
+  }
+
+  deleteEmployee(id:number): Observable<any>{
+    return this.http.get(USER_API +'delete/' +id);
+  }
 }
