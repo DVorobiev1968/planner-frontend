0bfbfb02fa69288c10f2bdaaa4b19277ade15a66
DVorobiev1968 <vdv@rdturbo.ru>
2021-12-23 16:49:54 +0500

    Настраиваем авторизацию auth-interceptor.service.ts

--- src/app/auth/helper/auth-interceptor.service.ts
+++ src/app/auth/helper/auth-interceptor.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import {HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {TokenStorageService} from "../../service/token-storage.service";
+
+const TOKEN_HEADER_KEY = 'Authorization'
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthInterceptorService {
+export class AuthInterceptorService implements HttpInterceptor{
 
-  constructor() { }
+
+  constructor(private tokenService: TokenStorageService) {
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return undefined;
+  }
 }
+export const authInterceptorProviders = [
+  {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}
+];
