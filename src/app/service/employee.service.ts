7bf106491cc916ce0fa9bad23490545f8b12ed0f
DVorobiev1968 <vdv@rdturbo.ru>
2021-12-23 17:47:40 +0500

    Пишем реализацию сервисов наших Entity: employee.service.ts notification.service.ts

--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {Employee} from "../models/Employee";
 
 const USER_API = 'http://localhost:8090/api/employee/';
 
@@ -9,16 +10,26 @@ const USER_API = 'http://localhost:8090/api/employee/';
 })
 export class EmployeeService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
+
   getEmployeeById(id: number): Observable<any> {
-    return this.http.get(USER_API +'id/' +id);
+    return this.http.get(USER_API + 'id/' + id);
   }
 
   updateEmployee(employee: any): Observable<any> {
     return this.http.post(USER_API + 'update', employee);
   }
 
-  deleteEmployee(id:number): Observable<any>{
-    return this.http.get(USER_API +'delete/' +id);
+  createEmployee(employee: Employee): Observable<any> {
+    return this.http.post(USER_API + 'add', employee);
+  }
+
+  deleteEmployee(id: number): Observable<any> {
+    return this.http.get(USER_API + 'delete/' + id);
+  }
+
+  listEmployee(): Observable<any> {
+    return this.http.get(USER_API + 'all');
   }
 }
