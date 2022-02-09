d0814fadcbd80049c82181924b97bcbe000cecad
DVorobiev1968 <vdv@rdturbo.ru>
2022-02-09 10:52:07 +0500

    Поправил навигацию. Подготовил первоначальные настройка для product, откатил angular.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,43 @@
 export const environment = {
-  production: true
+  production: {
+    "budgets": [
+      {
+        "type": "initial",
+        "maximumWarning": "2.4mb",
+        "maximumError": "5mb"
+      },
+      {
+        "type": "anyComponentStyle",
+        "maximumWarning": "2kb",
+        "maximumError": "4kb"
+      }
+    ],
+    "fileReplacements": [
+      {
+        "replace": "src/environments/environment.ts",
+        "with": "src/environments/environment.prod.ts"
+      }
+    ],
+    "outputHashing": "all"
+  },
+  development: {
+    "buildOptimizer": false,
+    "optimization": {
+      "scripts": true,
+      "styles": {
+        "minify": true,
+        "inlineCritical": false
+      },
+      "fonts": { "inline": true }
+    },
+    "vendorChunk": true,
+    "extractLicenses": false,
+    "sourceMap": {
+      "scripts": true,
+      "styles": false,
+      "hidden": true,
+      "vendor": true
+    },
+    "namedChunks": true
+  }
 };
