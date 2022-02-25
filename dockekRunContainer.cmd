8d9a3416c244fa9559064d7644bfb82e1dd90576
DVorobiev1968 <vdv@rdturbo.ru>
2022-02-25 14:21:04 +0500

    рефакторинг docker

---
+++ dockekRunContainer.cmd
@@ -0,0 +1,1 @@
+docker container run -h planning-front-end --name planning-front-end -d dvorobiev1968/planning-front-end
---
+++ dockerBuild.cmd
@@ -0,0 +1,1 @@
+docker build --add-host=planning-front-end:127.0.0.1 --add-host=planning-back-end:127.0.0.1 -t dvorobiev1968/planning-front-end .
