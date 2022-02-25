72821b6b9db0ba1ab3f6a6dca00cdff6caa6e656
DVorobiev1968 <vdv@rdturbo.ru>
2022-02-25 04:26:28 +0000

    Конфигурация product

--- src/app/service/Conf.ts
+++ src/app/service/Conf.ts
@@ -11,16 +11,16 @@ export class Conf{
   USER_API:string;
 
   constructor() {
-    this.AUTH_API='http://localhost:8090/api/auth/';
-    this.CATEGORY_API='http://localhost:8090/api/category/';
-    this.UPLOAD_API='http://localhost:8090/api/docs/';
-    this.EMPLOYEE_API = 'http://localhost:8090/api/employee/';
-    this.PRIORITY_API = 'http://localhost:8090/api/priority/';
-    this.ROUTE_TASK_API = 'http://localhost:8090/api/route/';
-    this.TASK_API = 'http://localhost:8090/api/task/';
+    this.AUTH_API='http://planning-back-end:8090/api/auth/';
+    this.CATEGORY_API='http://planning-back-end:8090/api/category/';
+    this.UPLOAD_API='http://planning-back-end:8090/api/docs/';
+    this.EMPLOYEE_API = 'http://planning-back-end:8090/api/employee/';
+    this.PRIORITY_API = 'http://planning-back-end:8090/api/priority/';
+    this.ROUTE_TASK_API = 'http://planning-back-end:8090/api/route/';
+    this.TASK_API = 'http://planning-back-end:8090/api/task/';
     this.TOKEN_KEY = 'auth-token';
     this.USER_KEY = 'auth-user';
-    this.USER_API = 'http://localhost:8090/api/user/';
+    this.USER_API = 'http://planning-back-end:8090/api/user/';
 
   }
 }
