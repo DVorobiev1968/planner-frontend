72e7a860f91debffff2d2e6ea42362883fc55e2b
Administrator <admin@example.com>
2022-04-12 15:26:37 +0500

    development

--- dockerBuild.cmd
+++ dockerBuild.cmd
@@ -1,1 +1,1 @@
-docker build --add-host=planning-front-end:127.0.0.1 --add-host=planning-back-end:127.0.0.1 -t dvorobiev1968/planning-front-end .
+docker build --add-host=planning-front-end:172.18.0.3 --add-host=planning-back-end:172.18.0.2 -t dvorobiev1968/planning-front-end .
