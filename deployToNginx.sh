7d964ccf874f7e03838bd0cabaacd1b65922ed79
Administrator <admin@example.com>
2022-05-20 11:14:08 +0500

    Рефакторинг deployToNginx.sh

--- deployToNginx.sh
+++ deployToNginx.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-# ng build --configuration production
 mkdir -p "/var/www/testedo.rdturbo.ru/html/planning-front-end"
-cp -r $PWD/dist/planning-front-end/* /var/www/testedo.rdturbo.ru/html/planning-front-end
+npm run build -- --configuration production --output-path=/var/www/testedo.rdturbo.ru/html/planning-front-end
